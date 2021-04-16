540bb482091cc8ff0fb32326ad29d46f2bbd8b31
antik52 <a.maksimenkovs@gmail.com>
2021-04-16 10:43:31 +0300

    Add: new model class Customer

---
+++ src/main/java/com/example/demo/models/Customer.java
@@ -0,0 +1,51 @@
+package com.example.demo.models;
+
+public class Customer {
+	
+	private int id;
+	private String name;
+	private String surname;
+	
+	private static int idCounter = 0;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = idCounter++;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	
+	public Customer(int id, String name, String surname) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", surname=" + surname + "]";
+	}
+	
+	
+	
+
+}
